9c414ddfeea33fa5337aee20eaeb0dbfc9fa0462
ryanngn61 <ryanmnguyen03@gmail.com>
2026-06-17 14:23:52 -0700

    Create test_sheet.py

---
+++ test_sheet.py
@@ -0,0 +1,24 @@
+python
+import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
+
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("Spotify Artists Watchlist").sheet1
+
+records = sheet.get_all_records()
+
+st.write("Connection successful!")
+st.write(records[:5])
+
