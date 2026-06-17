49653165ed6cd6132663162daf2a8845ec1fe6df
ryanngn61 <ryanmnguyen03@gmail.com>
2026-06-17 10:26:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,3 @@
 import streamlit as st
 
-from shuffle_playlist import shuffle_playlist
-from split_playlists import update_english_playlist
-
-st.title("Ryan's Spotify Tools")
-
-st.header("Update English Playlist")
-
-if st.button("Update English Playlist"):
-    update_english_playlist()
-    st.success("English playlist updated!")
-
-st.divider()
-
-st.header("Shuffle Playlist")
-
-playlist_link = st.text_input("Paste Spotify playlist link")
-
-if st.button("Shuffle Playlist"):
-
-    if playlist_link:
-        shuffle_playlist(playlist_link)
-        st.success("Playlist shuffled!")
-    else:
-        st.warning("Please paste a playlist link.")
+st.write(st.secrets.keys())
