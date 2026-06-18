389df9a3ea2120341589fa56737b92c3cf93353b
ryanngn61 <ryanmnguyen03@gmail.com>
2026-06-18 07:09:45 -0700

    Update sheet_utils.py

--- sheet_utils.py
+++ sheet_utils.py
@@ -1,3 +1,31 @@
-get_artists()
-add_artist()
-remove_artist()
+import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+def get_sheet():
+
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=scope
+    )
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open("Spotify Artists Watchlist")
+
+    return sheet.sheet1
+
+
+def get_artists():
+
+    worksheet = get_sheet()
+
+    rows = worksheet.get_all_records()
+
+    return rows
